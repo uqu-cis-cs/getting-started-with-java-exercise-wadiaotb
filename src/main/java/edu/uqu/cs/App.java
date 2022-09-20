65853c6d8f5b7e4c3b67bff76cb3ad369ca60528
wadiaotb <113444061+wadiaotb@users.noreply.github.com>
2022-09-20 18:51:40 +0300

    Update App.java
    My first git commit

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -15,6 +15,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello: wadia alot, s441013748" );
+        System.out.println( "Hello: wadia, s441013748" );
     }
 }
